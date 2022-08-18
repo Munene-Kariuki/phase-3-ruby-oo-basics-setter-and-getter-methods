f5e5cfa199d5a42e9b156fb4e790fd3744475897
Denis <denismunene999@gmail.com>
2022-08-18 16:51:26 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
+  #Setter method
+  def name=(name)
+    @name = name
+  end  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  #getter methods
+  def name 
+    @name
+  end
+  def breed
+    @breed
+  end    
 end
